228285b7135c6555cbb439b4238f04de3ab3ae62
Ayrat G <klub724@gmail.com>
2022-06-02 11:18:20 +0500

    hhtyjk

--- secret.js
+++ secret.js
@@ -1,6 +1,6 @@
 console.log('value is equal 10 : ', Number(process.env.SOME_SECRET) === 10 );
 console.log('value is < 10 : ', Number(process.env.SOME_SECRET) < 10 );
 console.log('value is > 10 : ', Number(process.env.SOME_SECRET) > 10 );
-if ( Number(process.env.SOME_SECRET) === 10) {
-    throw Error("Errorrr 10!!!!")
-}
+// if ( Number(process.env.SOME_SECRET) === 10) {
+//     throw Error("Errorrr 10!!!!")
+// }
