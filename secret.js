e05ab5b5c9e18e50aa55f6837b7cb7e785fd25df
Ayrat G <klub724@gmail.com>
2022-06-02 11:14:16 +0500

    bfgnhfg

--- secret.js
+++ secret.js
@@ -1,1 +1,3 @@
-console.log('value: ', process.env.SOME_SECRET);
+console.log('value is equal 10 : ', Number(process.env.SOME_SECRET) === 10 );
+console.log('value is < 10 : ', Number(process.env.SOME_SECRET) < 10 );
+console.log('value is > 10 : ', Number(process.env.SOME_SECRET) > 10 );
