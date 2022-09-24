aea42cf7650929a64e6c93a9c6f5b584802c864a
AyratLearnGIT <88925970+AyratLearnGIT@users.noreply.github.com>
2022-09-24 22:15:37 +0500

    test ws added (#29)
    * test ws added
    
    * indent
    
    * inside

---
+++ src/testWS.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestWs = () => {
+    return (
+        <div>
+            test ws
+            <div>inside</div>
+        </div>
+    );
+};
+
+export default TestWs;
