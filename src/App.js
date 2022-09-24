77b1912e137caefb8b35e244101660716e8cbc7c
AyratLearnGIT <88925970+AyratLearnGIT@users.noreply.github.com>
2022-09-24 22:14:42 +0500

    new test A (#32)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TestA from "./testA";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <TestA/>
       </header>
       <div>new b</div>
     </div>
